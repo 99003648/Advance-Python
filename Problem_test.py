e6bb12ce3a2d5704b76b4f480692cb159d9807e4
ANKIT KUMAR YADAV <78925345+99003648@users.noreply.github.com>
2021-02-20 17:35:28 +0530

    Update Problem_test.py

--- Problem_test.py
+++ Problem_test.py
@@ -12,11 +12,6 @@ def get_t2():
     return Temperature(22)
 
 
-def test_add(get_t1, get_t2):
-    res = get_t1 + get_t2
-    assert Temperature(57) == res
-
-
 def test_equal(get_t1, get_t2):
     assert (get_t1 == get_t2) == False
 
